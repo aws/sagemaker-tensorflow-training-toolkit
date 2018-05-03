35bca0617f3a5f2dc37f8ad444eff9235c72235a
Winston Dong <win@amazon.com>
2018-05-03 03:11:46 +0000

    Add accidentally removed file

---
+++ test/resources/iris/code/iris_no_serving_input.py
@@ -0,0 +1,53 @@
+#  Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#  
+#  Licensed under the Apache License, Version 2.0 (the "License").
+#  You may not use this file except in compliance with the License.
+#  A copy of the License is located at
+#  
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  
+#  or in the "license" file accompanying this file. This file is distributed 
+#  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+#  express or implied. See the License for the specific language governing 
+#  permissions and limitations under the License.
+
+import numpy as np
+import os
+import tensorflow as tf
+import logging
+
+logger = logging.getLogger(__name__)
+
+INPUT_TENSOR_NAME = 'inputs'
+
+
+def estimator_fn(run_config, params):
+    feature_columns = [tf.feature_column.numeric_column(INPUT_TENSOR_NAME, shape=[4])]
+    return tf.estimator.DNNClassifier(feature_columns=feature_columns,
+                                      hidden_units=[10, 20, 10],
+                                      n_classes=3,
+                                      config=run_config)
+
+
+def train_input_fn(training_dir, params):
+    """Returns input function that would feed the model during training"""
+    return _generate_input_fn(training_dir, 'iris_training.csv')
+
+
+def eval_input_fn(training_dir, params):
+    """Returns input function that would feed the model during evaluation"""
+    return _generate_input_fn(training_dir, 'iris_test.csv')
+
+
+def _generate_input_fn(training_dir, training_filename):
+
+    training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+        filename=os.path.join(training_dir, training_filename),
+        target_dtype=np.int,
+        features_dtype=np.float32)
+
+    return tf.estimator.inputs.numpy_input_fn(
+        x={INPUT_TENSOR_NAME: np.array(training_set.data)},
+        y=np.array(training_set.target),
+        num_epochs=None,
+        shuffle=True)()
