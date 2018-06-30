4de802e8ca2ae84b1c0c141f9407033840e2bdc3
Jack Mazanec <jmazane@amazon.com>
2018-06-30 12:14:47 -0700

    added ability to use script from any path

--- build_scripts/docker_image_creator.py
+++ build_scripts/docker_image_creator.py
@@ -5,6 +5,11 @@ Run the command:
 """
 import argparse
 import os
+import sys
+
+## GLOBALS
+# Allows user to run script anywhere
+BASE_PATH = ""
 
 def create_docker_image(optbin_link, processor, framework_version, python_version):
     """
@@ -24,15 +29,15 @@ def create_docker_image(optbin_link, processor, framework_version, python_versio
     # 2.) Get optimized binary - and put in final docker image repo
     print("Getting optimized binary...")
     optbin_filename = "{}-{}-cp27-cp27mu-manylinux1_x86_64.whl".format(framework, framework_version)
-    os.system("curl {} > ../docker/{}/final/{}/{}".format(optbin_link, framework_version, pyV, optbin_filename))
+    os.system("curl {} > {}/../docker/{}/final/{}/{}".format(BASE_PATH, optbin_link, framework_version, pyV, optbin_filename)) # ADDED BASE_PATH
     # 3.) Build base image
     print("Building base image...")
     image_name = "{}-base:{}-{}-{}".format(framework, framework_version, processor,  pyV)
-    base_docker_path = "../docker/{}/base/Dockerfile.{}".format(framework_version, processor)
+    base_docker_path = "{}/../docker/{}/base/Dockerfile.{}".format(framework_version, processor)  # ADDED BASE_PATH
     os.system("sudo nvidia-docker build -t {} -f {} .".format(image_name, base_docker_path))
     # 4.) Build final image
     print("Building final image...")
-    os.chdir("..")
+    os.chdir("{}/..".format(BASE_PATH)) # ADDED BASE_PATH
     os.system("python setup.py sdist")
     os.system("cp dist/sagemaker_tensorflow_container-*.tar.gz docker/{}/final/{}".format(framework_version, pyV))
     os.chdir("docker/{}/final/{}".format(framework_version, pyV))
@@ -40,15 +45,18 @@ def create_docker_image(optbin_link, processor, framework_version, python_versio
         ("sudo nvidia-docker build -t preprod-{}:{}-{}-{} --build-arg py_version={} --build-arg framework_installable={}  -f Dockerfile.{} .".format
             (framework, framework_version, processor, pyV, pyV[-1], optbin_filename, processor))
     # 5.) Return to build_scripts directory
-    os.chdir("../../../../build_scripts")
+    os.chdir("{}".format(BASE_PATH)) # ADDED BASE_PATH
 
 if __name__ == "__main__":
     # Parse command line options
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Build Sagemaker Tensorflow Docker Images")
     parser.add_argument("optimized_binary_link", help="link to place with optimized binary")
     parser.add_argument("processor_type", help="gpu if you would like to use GPUs or cpu")
     parser.add_argument("framework_version", help="Tensorflow framework version (i.e. 1.8.0)")
     parser.add_argument("python_version", help="Python version to be used (i.e. 2.7.0)")
     args = parser.parse_args()
+    # Sets BASE_PATH so that command can be run from anywhere
+    BASE_PATH = "/".join(sys.argv[0].split('/')[:-1])
+    BASE_PATH = "." if BASE_PATH == "" else BASE_PATH
     # Build image
     create_docker_image(args.optimized_binary_link, args.processor_type, args.framework_version, args.python_version)
