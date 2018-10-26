269f85aabf81b4ebb1c960b1dc56aefd26c359e8
Rui Wang <icywang.rui@gmail.com>
2018-10-26 13:40:40 -0700

    Add sagemaker integ tests

--- test/integration/sagemaker/test_mnist.py
+++ test/integration/sagemaker/test_mnist.py
@@ -14,7 +14,10 @@ from __future__ import absolute_import
 
 import os
 
+import pytest
 from sagemaker.tensorflow import TensorFlow
+
+from sagemaker_tensorflow_container.training import SAGEMAKER_PARAMETER_SERVER_NUM
 
 
 def test_mnist(sagemaker_session, ecr_image, instance_type):
@@ -35,13 +38,33 @@ def test_mnist(sagemaker_session, ecr_image, instance_type):
     estimator.fit(inputs)
 
 
-def test_distributed_mnist(sagemaker_session, ecr_image, instance_type):
+def test_distributed_mnist_no_ps(sagemaker_session, ecr_image, instance_type):
     resource_path = os.path.join(os.path.dirname(__file__), '../..', 'resources')
     script = os.path.join(resource_path, 'mnist', 'distributed_mnist.py')
     estimator = TensorFlow(entry_point=script,
                            role='SageMakerRole',
                            training_steps=1,
                            evaluation_steps=1,
+                           train_instance_count=2,
+                           train_instance_type=instance_type,
+                           sagemaker_session=sagemaker_session,
+                           image_name=ecr_image,
+                           base_job_name='test-tf-sm-distributed-mnist')
+    inputs = estimator.sagemaker_session.upload_data(
+        path=os.path.join(resource_path, 'mnist', 'data-distributed'),
+        key_prefix='scriptmode/mnist-distributed')
+    estimator.fit(inputs)
+
+
+@pytest.mark.parametrize('ps_num', [1, 2])
+def test_distributed_mnist_ps(sagemaker_session, ecr_image, instance_type, ps_num):
+    resource_path = os.path.join(os.path.dirname(__file__), '..', '..', 'resources')
+    script = os.path.join(resource_path, 'mnist', 'distributed_mnist.py')
+    estimator = TensorFlow(entry_point=script,
+                           role='SageMakerRole',
+                           training_steps=1,
+                           evaluation_steps=1,
+                           hyperparameters={SAGEMAKER_PARAMETER_SERVER_NUM: ps_num},
                            train_instance_count=2,
                            train_instance_type=instance_type,
                            sagemaker_session=sagemaker_session,
