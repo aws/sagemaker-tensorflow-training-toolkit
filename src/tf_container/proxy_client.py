aad4e7aa53e281f97edad3a56b3d5c1d525aa7cf
Dan Choi <choidan@amazon.com>
2018-10-30 19:06:31 -0700

    increase grpc timeout to 30

--- src/tf_container/proxy_client.py
+++ src/tf_container/proxy_client.py
@@ -1,14 +1,14 @@
 #  Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#  
+#
 #  Licensed under the Apache License, Version 2.0 (the "License").
 #  You may not use this file except in compliance with the License.
 #  A copy of the License is located at
-#  
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-#  
-#  or in the "license" file accompanying this file. This file is distributed 
-#  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
-#  express or implied. See the License for the specific language governing 
+#
+#  or in the "license" file accompanying this file. This file is distributed
+#  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+#  express or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
 import numpy as np
@@ -33,7 +33,7 @@ GENERIC_MODEL_NAME = "generic_model"
 
 
 class GRPCProxyClient(object):
-    def __init__(self, tf_serving_port, host='localhost', request_timeout=10.0,
+    def __init__(self, tf_serving_port, host='localhost', request_timeout=30.0,
                  model_name=GENERIC_MODEL_NAME,
                  input_tensor_name=PREDICT_INPUTS,
                  signature_name=DEFAULT_SERVING_SIGNATURE_DEF_KEY):
