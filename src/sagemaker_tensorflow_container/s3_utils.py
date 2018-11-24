49a054751738fbafe515c30d10079714d3883192
Marcio Vinicius dos Santos <mvs.usp@gmail.com>
2018-11-24 11:18:17 -0500

    Add Keras support (#126)
    * Add Keras support

--- src/sagemaker_tensorflow_container/s3_utils.py
+++ src/sagemaker_tensorflow_container/s3_utils.py
@@ -20,22 +20,24 @@ from six.moves.urllib.parse import urlparse
 
 def configure(model_dir, job_region):
 
-    if not model_dir:
-        return
-
-    s3 = boto3.client('s3', region_name=job_region)
-
-    # We get the AWS region of the checkpoint bucket, which may be different from
-    # the region this container is currently running in.
-    parsed_url = urlparse(model_dir)
-    bucket_name = parsed_url.netloc
-
-    bucket_location = s3.get_bucket_location(Bucket=bucket_name)['LocationConstraint']
-
-    # Configure environment variables used by TensorFlow S3 file system
-    if bucket_location:
-        os.environ['S3_REGION'] = bucket_location
+    os.environ['S3_REGION'] = _s3_region(job_region, model_dir)
 
     # setting log level to WARNING
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
     os.environ['S3_USE_HTTPS'] = '1'
+
+
+def _s3_region(job_region, model_dir):
+    if model_dir and model_dir.startswith('s3://'):
+        s3 = boto3.client('s3', region_name=job_region)
+
+        # We get the AWS region of the checkpoint bucket, which may be different from
+        # the region this container is currently running in.
+        parsed_url = urlparse(model_dir)
+        bucket_name = parsed_url.netloc
+
+        bucket_location = s3.get_bucket_location(Bucket=bucket_name)['LocationConstraint']
+
+        return bucket_location or job_region
+    else:
+        return job_region
