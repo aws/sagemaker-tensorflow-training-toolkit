62cae3c06b35905cd3a8abf665c259cba15d872e
Rui Wang <icywang.rui@gmail.com>
2018-12-05 14:26:02 -0800

    Remove port randomization

--- test/integration/local/test_keras.py
+++ test/integration/local/test_keras.py
@@ -45,10 +45,6 @@ def test_keras_training(sagemaker_local_session, docker_image, tmpdir):
 
     estimator.fit()
 
-    # Use random port to avoid port collision
-    random_port = randint(1025, 65535)
-    sagemaker_local_session.config = {'local': {'serving_port': random_port}}
-    sagemaker_local_session.sagemaker_runtime_client.serving_port = random_port
     model = serving.Model(model_data=output_path,
                           role='SageMakerRole',
                           framework_version='1.11.0',
