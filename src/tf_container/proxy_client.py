24d19c4246df51f08fbf41212825b5ee70e9f28b
Dan Choi <choidan@amazon.com>
2018-12-12 15:59:34 -0800

    make grpc tfs request timeout configurable

--- src/tf_container/proxy_client.py
+++ src/tf_container/proxy_client.py
@@ -27,6 +27,7 @@ from tensorflow_serving.apis import prediction_service_pb2
 from tf_container.run import logger as _logger
 
 INFERENCE_ACCELERATOR_PRESENT_ENV = 'SAGEMAKER_INFERENCE_ACCELERATOR_PRESENT'
+TF_SERVING_REQUEST_TIMEOUT_ENV = 'SAGEMAKER_TFS_GRPC_REQUEST_TIMEOUT'
 
 REGRESSION = 'tensorflow/serving/regression'
 CLASSIFY = 'tensorflow/serving/classify'
@@ -40,7 +41,9 @@ class GRPCProxyClient(object):
                  model_name=GENERIC_MODEL_NAME,
                  input_tensor_name=PREDICT_INPUTS,
                  signature_name=DEFAULT_SERVING_SIGNATURE_DEF_KEY):
-        if os.environ.get(INFERENCE_ACCELERATOR_PRESENT_ENV) == 'true':
+        if os.environ.get(TF_SERVING_REQUEST_TIMEOUT_ENV):
+            request_timeout = int(TF_SERVING_REQUEST_TIMEOUT_ENV)
+        elif os.environ.get(INFERENCE_ACCELERATOR_PRESENT_ENV) == 'true':
             request_timeout = 30.0
 
         self.tf_serving_port = tf_serving_port
