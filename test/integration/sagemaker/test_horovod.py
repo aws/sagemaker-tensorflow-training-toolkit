02a9ee45fe15678d7df6ba4b6996844e37da63a7
Marcio Dos Santos <mvs@amazon.com>
2018-12-20 09:50:18 -0500

    Test fix

--- test/integration/sagemaker/test_horovod.py
+++ test/integration/sagemaker/test_horovod.py
@@ -49,10 +49,6 @@ def test_distributed_training_cpu_horovod(instances,
 
     estimator.fit(inputs)
 
-    _assert_s3_files_exist(os.path.join(estimator.model_dir, 'checkpoints'),
-                           ['model.ckpt-50.meta', 'model.ckpt-50.data-00000-of-00001',
-                            'model.ckpt-50.index'])
-
 
 @pytest.mark.parametrize('instances, processes', [
     (5, 1)])
@@ -80,32 +76,3 @@ def test_distributed_training_gpu_horovod(instances,
         key_prefix='scriptmode/mnist')
 
     estimator.fit(inputs)
-
-    _assert_s3_files_exist(estimator.model_dir,
-                           ['model.ckpt-40.meta', 'model.ckpt-40.data-00000-of-00001',
-                            'model.ckpt-40.index'])
-
-
-def _assert_s3_files_exist(s3_url, files):
-    parsed_url = urlparse(s3_url)
-    s3 = boto3.client('s3')
-    contents = s3.list_objects_v2(Bucket=parsed_url.netloc, Prefix=parsed_url.path.lstrip('/'))["Contents"]
-    for f in files:
-        found = [x['Key'] for x in contents if x['Key'].endswith(f)]
-        if not found:
-            raise ValueError('File {} is not found under {}'.format(f, s3_url))
-
-
-def _assert_s3_files_exist_in_tar(s3_url, files):
-    parsed_url = urlparse(s3_url)
-    tmp_file = tempfile.NamedTemporaryFile()
-    s3 = boto3.resource('s3')
-    object = s3.Bucket(parsed_url.netloc).Object(parsed_url.path.lstrip('/'))
-
-    with open(tmp_file.name, 'wb') as temp_file:
-        object.download_fileobj(temp_file)
-        with tarfile.open(tmp_file.name, 'r') as tar_file:
-            for f in files:
-                found = [x for x in tar_file.getnames() if x.endswith(f)]
-                if not found:
-                    raise ValueError('File {} is not found in {}'.format(f, s3_url))
