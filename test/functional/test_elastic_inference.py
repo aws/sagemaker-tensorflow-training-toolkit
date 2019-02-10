7f2f9603ac409948e9351faffee1d721f11db1a4
Dan Choi <choidan@amazon.com>
2019-02-10 15:10:24 -0800

    delete endpoint in integ test

--- test/functional/test_elastic_inference.py
+++ test/functional/test_elastic_inference.py
@@ -17,10 +17,11 @@ import os
 import numpy as np
 import pytest
 from sagemaker.tensorflow import TensorFlowModel
+from sagemaker.utils import sagemaker_timestamp
 
 from test.functional import EI_SUPPORTED_REGIONS
 from test.integ.conftest import SCRIPT_PATH
-from test.resources.python_sdk.timeout import timeout
+from test.resources.python_sdk.timeout import timeout_and_delete_endpoint_by_name
 
 logger = logging.getLogger(__name__)
 logging.getLogger('boto3').setLevel(logging.INFO)
@@ -41,7 +42,7 @@ def skip_if_no_accelerator(accelerator_type):
 @pytest.fixture(autouse=True)
 def skip_if_non_supported_ei_region(region):
     if region not in EI_SUPPORTED_REGIONS:
-        pytest.skip('EI is not supported in :'.format(region))
+        pytest.skip('EI is not supported in {}'.format(region))
 
 
 @pytest.fixture
@@ -49,22 +50,27 @@ def pretrained_model_data(region):
     return 's3://sagemaker-sample-data-{}/tensorflow/model/resnet/resnet_50_v2_fp32_NCHW.tar.gz'.format(region)
 
 
+# based on https://github.com/awslabs/amazon-sagemaker-examples/blob/master/sagemaker-python-sdk/tensorflow_using_elastic_inference_with_your_own_model/tensorflow_pretrained_model_elastic_inference.ipynb
 @pytest.mark.skip_if_non_supported_ei_region
 @pytest.mark.skip_if_no_accelerator
-# based on https://github.com/awslabs/amazon-sagemaker-examples/blob/master/sagemaker-python-sdk/tensorflow_using_elastic_inference_with_your_own_model/tensorflow_pretrained_model_elastic_inference.ipynb
-def test_elastic_inference(pretrained_model_data, docker_image_uri, instance_type, accelerator_type):
+def test_deploy_elastic_inference_with_pretrained_model(pretrained_model_data, docker_image_uri, sagemaker_session, instance_type, accelerator_type):
     resource_path = os.path.join(SCRIPT_PATH, '../resources')
-    with timeout(minutes=20):
+    endpoint_name = 'test-tf-ei-deploy-model-{}'.format(sagemaker_timestamp())
+
+    with timeout_and_delete_endpoint_by_name(endpoint_name=endpoint_name, sagemaker_session=sagemaker_session,
+                                             minutes=20):
         tensorflow_model = TensorFlowModel(model_data=pretrained_model_data,
                                            entry_point='default_entry_point.py',
                                            source_dir=resource_path,
                                            role='SageMakerRole',
-                                           image=docker_image_uri)
+                                           image=docker_image_uri,
+                                           sagemaker_session=sagemaker_session)
 
         logger.info("deploy model")
         predictor = tensorflow_model.deploy(initial_instance_count=1,
                                             instance_type=instance_type,
-                                            accelerator_type=accelerator_type)
+                                            accelerator_type=accelerator_type,
+                                            endpoint_name=endpoint_name)
 
         random_input = np.random.rand(1, 1, 3, 3)
 
