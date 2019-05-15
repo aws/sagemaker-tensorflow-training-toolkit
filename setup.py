fb1fbdf858185f7044bd909e786deff0ea0b0bd5
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-05-15 10:14:54 -0700

    Explicitly set lower-bound for botocore version (#187)

--- setup.py
+++ setup.py
@@ -52,8 +52,8 @@ setup(
     install_requires=['sagemaker-containers>=2.4.6', 'numpy', 'scipy', 'sklearn',
                       'pandas', 'Pillow', 'h5py'],
     extras_require={
-        'test': ['tox', 'flake8', 'pytest==4.4.1', 'pytest-cov', 'pytest-xdist', 'mock',
-                 'sagemaker==1.19.1', 'tensorflow', 'docker-compose'],
+        'test': ['tox', 'flake8', 'pytest', 'pytest-cov', 'pytest-xdist', 'mock',
+                 'sagemaker==1.19.1', 'tensorflow', 'docker-compose', 'botocore>=1.12.140'],
         'benchmark': ['click']
     },
 )
