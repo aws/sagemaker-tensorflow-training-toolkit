b40133d79db1074b12a209c31ffdda2cef82a5ba
Rui Wang <icywang.rui@gmail.com>
2019-07-08 14:28:05 -0700

    Flip master to script-mode

--- setup.py
+++ setup.py
@@ -1,19 +1,38 @@
-import os
+# Copyright 2017-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+from __future__ import absolute_import
+
 from glob import glob
+import os
 from os.path import basename
 from os.path import splitext
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
+def read_version():
+    return read('VERSION').strip()
+
+
 setup(
     name='sagemaker_tensorflow_container',
-    version='1.0.0',
-    description='Open source library for creating TensorFlow containers to run on Amazon SageMaker.',
+    version=read_version(),
+    description='Open source library for creating '
+                'TensorFlow containers to run on Amazon SageMaker.',
 
     packages=find_packages(where='src', exclude=('test',)),
     package_dir={'': 'src'},
@@ -31,12 +50,14 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
-    install_requires=['sagemaker-container-support'],
+    install_requires=['sagemaker-containers>=2.4.10', 'numpy', 'scipy', 'sklearn',
+                      'pandas', 'Pillow', 'h5py'],
     extras_require={
-        'test': ['tox', 'flake8', 'pytest==4.5.0', 'pytest-cov', 'pytest-xdist', 'mock',
-                 'pluggy==0.11', 'sagemaker', 'tensorflow', 'tensorflow-serving-api']
+        'test': ['tox', 'flake8', 'pytest==4.4.1', 'pytest-cov', 'pytest-xdist', 'mock',
+                 'sagemaker==1.19.1', 'tensorflow', 'docker-compose', 'botocore>=1.12.140'],
+        'benchmark': ['click']
     },
 )
