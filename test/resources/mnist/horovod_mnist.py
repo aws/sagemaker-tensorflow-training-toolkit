d269e570a529186f250327a94375974abcc5442d
Chuyang <chuyangdeng@gmail.com>
2019-12-12 11:04:15 -0800

    change: tensorflow-2.0 tests
    * fix: tensorflow-2.0 library code changes
    
    * remove >=2.0 off tensorflow restrictions
    
    * fix: update mnist scripts for tf-2.0
    
    * add dockerfiles

--- test/resources/mnist/horovod_mnist.py
+++ test/resources/mnist/horovod_mnist.py
@@ -29,7 +29,7 @@ import horovod.keras as hvd
 hvd.init()
 
 # Horovod: pin GPU to be used to process local rank (one GPU per process)
-config = tf.ConfigProto()
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 config.gpu_options.visible_device_list = str(hvd.local_rank())
 K.set_session(tf.Session(config=config))
