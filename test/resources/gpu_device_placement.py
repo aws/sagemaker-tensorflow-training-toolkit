95b1cba6e1b1a2fa90ae80443e20441650f2987d
Chuyang <chuyangdeng@gmail.com>
2019-12-12 20:02:25 -0800

    fix: update scripts to support tf-2.0 (#250)

--- test/resources/gpu_device_placement.py
+++ test/resources/gpu_device_placement.py
@@ -19,7 +19,7 @@ with tf.device('/gpu:0'):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
 c = tf.matmul(a, b)
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
 # Runs the op.
 print(sess.run(c))
 print('-' * 87)
