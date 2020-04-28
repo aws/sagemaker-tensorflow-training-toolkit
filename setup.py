5455c5ef50fb9d743f5ce982476fd420af43d783
Ajay Karpur <akarpur@amazon.com>
2020-04-28 16:20:49 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
 
