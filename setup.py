46325ee5ace739a8b0a7d18933c7f2d7905489f8
Ajay Karpur <akarpur@amazon.com>
2020-05-06 16:22:20 -0700

    feature: Python 3.7 support (#362)
    * infra: Test against py37 in unit tests
    
    * infra: Add py37 coverage file

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         "Programming Language :: Python",
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     install_requires=['sagemaker-training>=3.5.0', 'numpy', 'scipy', 'sklearn',
