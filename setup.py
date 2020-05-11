28445f7b5fdfb08d03ca4ade8fc9c731a9ee9a3a
Ajay Karpur <akarpur@amazon.com>
2020-05-11 12:47:51 -0700

    bump version of sagemaker-training for typing fix

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
         'Programming Language :: Python :: 3.7',
     ],
 
-    install_requires=['sagemaker-training>=3.5.0', 'numpy', 'scipy', 'sklearn',
+    install_requires=['sagemaker-training>=3.5.1', 'numpy', 'scipy', 'sklearn',
                       'pandas', 'Pillow', 'h5py'],
     extras_require={
         'test': test_dependencies,
