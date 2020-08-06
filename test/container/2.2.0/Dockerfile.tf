f2fd85d06d7db52df7486efaab7fe0cc60f1b25c
Chuyang <chuyangdeng@gmail.com>
2020-08-05 20:30:27 -0700

    feature: tensorflow 2.3 support (#397)
    * feature: tensorflow 2.3 support
    
    * update test container framework versions
    
    * use tf training 2.2 py3 image
    
    * update dlc image tag to py37
    
    Co-authored-by: Chuyang Deng <chuyangd@amazon.com>

--- test/container/2.2.0/Dockerfile.tf
+++ test/container/2.2.0/Dockerfile.tf
@@ -1,5 +1,6 @@
-ARG region
-FROM 763104351884.dkr.ecr.$region.amazonaws.com/tensorflow-training:2.1.0-gpu-py3
+FROM tensorflow/tensorflow:2.3.0-gpu
+
+ENV SAGEMAKER_TRAINING_MODULE sagemaker_tensorflow_container.training:main
 
 COPY dist/sagemaker_tensorflow_training-*.tar.gz /sagemaker_tensorflow_training.tar.gz
 RUN pip install --upgrade --no-cache-dir /sagemaker_tensorflow_training.tar.gz && \
