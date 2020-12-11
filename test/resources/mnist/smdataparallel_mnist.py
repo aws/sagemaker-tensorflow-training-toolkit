ed9f68fcfc80a5a9b2e84fd951a538f7ce7aaf21
Dan Choi <choidan@amazon.com>
2020-12-11 06:48:41 -0800

    use user script from Python SDK data parallel integ test

--- test/resources/mnist/smdataparallel_mnist.py
+++ test/resources/mnist/smdataparallel_mnist.py
@@ -10,159 +10,74 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-import argparse
-import errno
-import os
-
-import numpy as np
-import smdistributed.dataparallel.tensorflow as dist
 import tensorflow as tf
-from tensorflow import keras
+
+import smdistributed.dataparallel.tensorflow as dist
+
+tf.random.set_seed(42)
 
 dist.init()
 
-layers = tf.layers
+gpus = tf.config.experimental.list_physical_devices("GPU")
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+if gpus:
+    tf.config.experimental.set_visible_devices(gpus[dist.local_rank()], "GPU")
 
-tf.logging.set_verbosity(tf.logging.INFO)
+(mnist_images, mnist_labels), _ = tf.keras.datasets.mnist.load_data(
+    path="mnist-%d.npz" % dist.rank()
+)
 
+dataset = tf.data.Dataset.from_tensor_slices(
+    (tf.cast(mnist_images[..., tf.newaxis] / 255.0, tf.float32), tf.cast(mnist_labels, tf.int64))
+)
+dataset = dataset.repeat().shuffle(10000).batch(128)
 
-def conv_model(feature, target, mode):
-    """2-layer convolution model."""
-    # Convert the target to a one-hot tensor of shape (batch_size, 10) and
-    # with a on-value of 1 for each one-hot vector of length 10.
-    target = tf.one_hot(tf.cast(target, tf.int32), 10, 1, 0)
-
-    # Reshape feature to 4d tensor with 2nd and 3rd dimensions being
-    # image width and height final dimension being the number of color channels.
-    feature = tf.reshape(feature, [-1, 28, 28, 1])
-
-    # First conv layer will compute 32 features for each 5x5 patch
-    with tf.variable_scope("conv_layer1"):
-        h_conv1 = layers.conv2d(
-            feature, 32, kernel_size=[5, 5], activation=tf.nn.relu, padding="SAME"
-        )
-        h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
-
-    # Second conv layer will compute 64 features for each 5x5 patch.
-    with tf.variable_scope("conv_layer2"):
-        h_conv2 = layers.conv2d(
-            h_pool1, 64, kernel_size=[5, 5], activation=tf.nn.relu, padding="SAME"
-        )
-        h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
-        # reshape tensor into a batch of vectors
-        h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-
-    # Densely connected layer with 1024 neurons.
-    h_fc1 = layers.dropout(
-        layers.dense(h_pool2_flat, 1024, activation=tf.nn.relu),
-        rate=0.5,
-        training=mode == tf.estimator.ModeKeys.TRAIN,
-    )
-
-    # Compute logits (1 per class) and compute loss.
-    logits = layers.dense(h_fc1, 10, activation=None)
-    loss = tf.losses.softmax_cross_entropy(target, logits)
-
-    return tf.argmax(logits, 1), loss
-
-
-def train_input_generator(x_train, y_train, batch_size=64):
-    assert len(x_train) == len(y_train)
-    while True:
-        p = np.random.permutation(len(x_train))
-        x_train, y_train = x_train[p], y_train[p]
-        index = 0
-        while index <= len(x_train) - batch_size:
-            yield x_train[index : index + batch_size], y_train[index : index + batch_size],
-            index += batch_size
-
-
-def main(_):
-    """Download dataset and build the model.
-
-    Note:
-    Keras automatically creates a cache directory in ~/.keras/datasets for
-    storing the downloaded MNIST data. This creates a race
-    condition among the workers that share the same filesystem. If the
-    directory already exists by the time this worker gets around to creating
-    it, ignore the resulting exception and continue.
-    """
-    # Training settings
-    parser = argparse.ArgumentParser(description='Tensorflow MNIST Example')
-    parser.add_argument('model_dir', type=str, default=os.environ['SM_MODEL_DIR'])
-    args = parser.parse_args()
-
-    cache_dir = os.path.join(os.path.expanduser("~"), ".keras", "datasets")
-    if not os.path.exists(cache_dir):
-        try:
-            os.mkdir(cache_dir)
-        except OSError as e:
-            if e.errno == errno.EEXIST and os.path.isdir(cache_dir):
-                pass
-            else:
-                raise
-
-    # Download and load MNIST dataset.
-    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data(f"MNIST-data-{dist.rank()}")
-
-    # The shape of downloaded data is (-1, 28, 28), hence we need to reshape it
-    # into (-1, 784) to feed into our network. Also, need to normalize the
-    # features between 0 and 1.
-    x_train = np.reshape(x_train, (-1, 784)) / 255.0
-    x_test = np.reshape(x_test, (-1, 784)) / 255.0
-
-    # Build model...
-    with tf.name_scope("input"):
-        image = tf.placeholder(tf.float32, [None, 784], name="image")
-        label = tf.placeholder(tf.float32, [None], name="label")
-    predict, loss = conv_model(image, label, tf.estimator.ModeKeys.TRAIN)
-
-    lr_scaler = dist.size()
-
-    # SMDataParallel: adjust learning rate based on lr_scaler.
-    opt = tf.train.AdamOptimizer(0.001 * lr_scaler)
-
-    # SMDataParallel: add SMDataParallel Distributed Optimizer.
-    opt = dist.DistributedOptimizer(opt)
-
-    global_step = tf.train.get_or_create_global_step()
-    train_op = opt.minimize(loss, global_step=global_step)
-
-    hooks = [
-        # SMDataParallel: BroadcastGlobalVariablesHook broadcasts initial variable states
-        # from rank 0 to all other processes. This is necessary to ensure consistent
-        # initialization of all workers when training is started with random weights
-        # or restored from a checkpoint.
-        dist.BroadcastGlobalVariablesHook(0),
-        # SMDataParallel: adjust number of steps based on number of GPUs.
-        tf.train.StopAtStepHook(last_step=20000 // dist.size()),
+mnist_model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Conv2D(32, [3, 3], activation="relu"),
+        tf.keras.layers.Conv2D(64, [3, 3], activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(10, activation="softmax"),
     ]
+)
+loss = tf.losses.SparseCategoricalCrossentropy()
+# LR for 8 node run : 0.000125
+# LR for single node run : 0.001
+opt = tf.optimizers.Adam(0.000125 * dist.size())
 
-    if dist.rank() == 0:
-        hooks.append(
-            tf.train.LoggingTensorHook(tensors={"step": global_step, "loss": loss}, every_n_iter=10)
-        )
-
-    # SMDataParallel: pin GPU to be used to process local rank (one GPU per process)
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    config.gpu_options.visible_device_list = str(dist.local_rank())
-
-    # SMDataParallel: save checkpoints only on worker 0 to prevent other workers from
-    # corrupting them.
-    checkpoint_dir = args.model_dir if dist.rank() == 0 else None
-    training_batch_generator = train_input_generator(x_train, y_train, batch_size=100)
-    # The MonitoredTrainingSession takes care of session initialization,
-    # restoring from a checkpoint, saving to a checkpoint, and closing when done
-    # or an error occurs.
-    with tf.train.MonitoredTrainingSession(
-        checkpoint_dir=checkpoint_dir, hooks=hooks, config=config
-    ) as mon_sess:
-        while not mon_sess.should_stop():
-            # Run a training step synchronously.
-            image_, label_ = next(training_batch_generator)
-            mon_sess.run(train_op, feed_dict={image: image_, label: label_})
+checkpoint_dir = "./checkpoints"
+checkpoint = tf.train.Checkpoint(model=mnist_model, optimizer=opt)
 
 
-if __name__ == "__main__":
-    tf.app.run()
+@tf.function
+def training_step(images, labels, first_batch):
+    with tf.GradientTape() as tape:
+        probs = mnist_model(images, training=True)
+        loss_value = loss(labels, probs)
+
+    tape = dist.DistributedGradientTape(tape)
+
+    grads = tape.gradient(loss_value, mnist_model.trainable_variables)
+    opt.apply_gradients(zip(grads, mnist_model.trainable_variables))
+
+    if first_batch:
+        dist.broadcast_variables(mnist_model.variables, root_rank=0)
+        dist.broadcast_variables(opt.variables(), root_rank=0)
+
+    loss_value = dist.oob_allreduce(loss_value)  # Average the loss across workers
+    return loss_value
+
+
+for batch, (images, labels) in enumerate(dataset.take(10000 // dist.size())):
+    loss_value = training_step(images, labels, batch == 0)
+
+    if batch % 50 == 0 and dist.rank() == 0:
+        print("Step #%d\tLoss: %.6f" % (batch, loss_value))
+
+if dist.rank() == 0:
+    checkpoint.save(checkpoint_dir)
