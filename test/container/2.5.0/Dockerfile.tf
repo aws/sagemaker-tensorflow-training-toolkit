d45fc59b5c723f9b53da0f37dfb0de894a34db48
Satish Pasumarthi <spasuma@amazon.com>
2022-06-01 12:46:39 -0700

    test with tf.25 and py37

--- test/container/2.5.0/Dockerfile.tf
+++ test/container/2.5.0/Dockerfile.tf
@@ -1,5 +1,6 @@
-ARG region
-FROM 763104351884.dkr.ecr.$region.amazonaws.com/tensorflow-training:2.8.0-gpu-py39-cu112-ubuntu20.04-sagemaker
+FROM tensorflow/tensorflow:2.5.0-gpu
+
+ENV SAGEMAKER_TRAINING_MODULE sagemaker_tensorflow_container.training:main
 
 COPY dist/sagemaker_tensorflow_training-*.tar.gz /sagemaker_tensorflow_training.tar.gz
 RUN pip install --upgrade --no-cache-dir /sagemaker_tensorflow_training.tar.gz && \
